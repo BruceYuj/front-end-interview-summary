35e6e28b776db4379013cd15038c783a9da791cd
Bruce <444048170@qq.com>
2020-03-25 13:48:08 +0800

    update leetcode

--- algorithms/leetcode-problems/debug/demo.js
+++ algorithms/leetcode-problems/debug/demo.js
@@ -1,12 +1,37 @@
-const backPack = function (m, A) {
-    let n = A.length;
-    let dp = new Array(m+1).fill(0);
-    
-    for(let i = 0; i < n; i++)
-        for(let j = 1; j <= m; j++) {
-            dp[j] = Math.max(dp[j], j >= A[i] ?(dp[j-A[i]] + A[i]) : 0);
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minCut = function(s) {
+    let dp = new Array(s.length).fill(s.length);
+    let palindrome = new Array(s.length);
+    for(let i = 0; i < s.length; i++) palindrome[i] = new Array(s.length).fill(false);
+
+    for(let i = 0; i < s.length; i++) {
+        for(let j = 0; j < s.length; j++) {
+            if(j <= i) {
+                palindrome[i][j] = true;
+                continue;
+            }
+            palindrome[i][j] =  s[i] === s[j] && palindrome[i+1][j-1];
         }
-        
-    return dp[m];
-}
-backPack(10, [3,4,8,5]);
+    }
+
+    dp[0] = 0;
+    for(let i = 1; i < dp.length; i++) {
+        if(palindrome[0][i]) {
+            dp[i] = 0;
+            continue;
+        }
+        for(let j = 0; j < i; j++) {
+
+            if(palindrome[j+1][i]) {
+                dp[i] = Math.min(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    return dp[s.length-1];
+};
+
+minCut("aabbc");

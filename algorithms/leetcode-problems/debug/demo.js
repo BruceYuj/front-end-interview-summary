3d27b7cf7bf7e9cba890eb7a43e05b4c706b1041
Bruce <444048170@qq.com>
2020-03-26 15:14:49 +0800

    update leetcode

--- algorithms/leetcode-problems/debug/demo.js
+++ algorithms/leetcode-problems/debug/demo.js
@@ -7,28 +7,41 @@
 
 var wordBreak = function(s, wordDict) {
     let map = {};
-    for(let i = 0; i < wordDict.length; i++) {
-        map[wordDict[i]] = true;
-    }
-    let dp = new Array(s.length+1);
-    for(let i = 0; i < s.length; i++) dp[i] = [];
+    for(let i = 0; i < wordDict.length; i++) map[wordDict[i]] = true;
 
-    dp[s.length] = [''];
+    let memo = new Array(s.length).fill(-1);
 
-    for(let i = s.length - 1; i >= 0; i--) {
+    let ans = helper(0);
+    return ans[1].map(elem => {
+        return elem.join(' ');
+    })
+
+    function helper(index) {
+        if(index >= s.length) return [true, []];
+        if(memo[index] !== -1) return memo[index];
+
         let path = [];
-        for(let j = i; j < s.length; j++) {
-            let word = s.slice(i, j+1);
-            if( map[word]) {
-                for(let k = 0; k < dp[j+1].length; k++) {
-                    let tmp = word + (dp[j+1][k].length === 0 ? '':' ') +  dp[j+1][k]; 
-                    path.push(tmp);
+        let isSucceed = false;
+        for(let i = index; i < s.length; i++) {
+            let word = s.slice(index, i+1);
+            if(map[word]) {
+                let tmp = helper(i+1);
+                if(tmp[0]) {
+                    isSucceed = true;
+                    if(!tmp[1].length) {
+                        path.push([word]);
+                        continue;
+                    }
+                    for(let j = 0; j < tmp[1].length; j++) {
+                        path.push([word, ...tmp[1][j]]);
+                    }
                 }
             }
         }
-        dp[i] =  path;
+
+        memo[index] = [isSucceed, path];
+        return memo[index];
     }
-    return dp[0];
 };
 
 wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"])

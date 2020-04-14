c4d0d09a16595fcada8a27f10d21ae9cc9333e9a
bruceyuj <444048170@qq.com>
2020-04-14 21:46:37 +0800

    add a new series - leetcode summary series

---
+++ commit.sh
@@ -0,0 +1,18 @@
+# git status | grep modified | awk '{print $2}' | sed '1a drink tea'
+
+
+# sed -i '1i\ ' README.md
+# current=`date` && sed -i "1c **文本更新于 $current**" README.md
+
+ # 1. 给每个 md 文件开头新加一个空行
+# for file in `find  -name "*.md"`
+# do
+#     sed -i '1i\ ' $file
+# done
+
+git add .
+for file in `git diff HEAD --name-only`
+do
+    current=`date` && sed -i "1c **文本更新于 $current**" $file
+done
+

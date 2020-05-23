bd4f9533a806e7954f9e839c3d7efbfea35ce400
bruceyuj <444048170@qq.com>
2020-05-23 17:10:19 +0800

    add leetcode series

--- algorithms/leetcode/debug/demo.js
+++ algorithms/leetcode/debug/demo.js
@@ -1,61 +1,16 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
+var integerReplacement = function(n) {
+    let count = 0;
 
-var longestZigZag = function(root) {
-    let map = {};
-    let map2 = {};
-
-    return dfs(root);
-
-    function dfs(root) {
-        if(!root) return 0;
-
-
-        let left = helper(root.left, true);
-        let right = helper(root.right, false);
-
-        left = Math.max(left, dfs(root.left));
-        right = Math.max(right, dfs(root.right));
-
-        return Math.max(left, right); 
-    }
-
-
-    function helper(node, direction) {
-        if(!node) return 0;
-        if(direction) {
-            if(map2[node]) return 1 + map2[node];
-            map2[node] = helper(node.right, !direction);
-            return 1+map2[node];
-        } else{
-            if(map[node]) return 1+map[node];
-            map[node] = helper(node.left, !direction);
-
-            return 1+map[node];
+    while(n !== 1) {
+        if((n & 1) === 0) n >>>= 1;
+        else {
+            if((n & 2) === 0 || n === 3) n -= 1;
+            else n += 1;
         }
+        count++;
     }
+
+    return count;
 };
 
-let root = makeTreeFromArr([1,1,1,1,null,1,null,null,1,1]);
-
-console.log(longestZigZag(root));
+console.log(integerReplacement(2147483647));

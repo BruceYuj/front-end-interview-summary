9d167ad33c098585dbe680de139c2f711a1bb4d9
Bruce <444048170@qq.com>
2020-05-28 11:02:02 +0800

    add leetcode series

--- algorithms/leetcode/debug/demo.js
+++ algorithms/leetcode/debug/demo.js
@@ -1,16 +1,59 @@
-var integerReplacement = function(n) {
-    let count = 0;
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    let n = s.length;
+    let num = '';
+    let encoded = '';
+    let stack = [];
 
-    while(n !== 1) {
-        if((n & 1) === 0) n >>>= 1;
-        else {
-            if((n & 2) === 0 || n === 3) n -= 1;
-            else n += 1;
+    let ans = '';
+
+    for(let i = 0; i < n; i++) {
+        if(s[i] === ']') {
+            let status = 0;
+            while(stack.length) {
+                let top = stack.pop();
+                let code = top.charCodeAt(0);
+                if(top === '[' && status === 0) {
+                    status = 1;
+                    continue;
+                }
+                if(code >= 48 && code <= 57 && status === 1) {
+                    status = 2;
+                    num= top + num;
+                    continue;
+                }
+                if(status === 0)encoded = top + encoded;
+                else {
+                    stack.push(top);
+                    break;
+                }
+            }
+            let count = parseInt(num);
+            if(num === '' && encoded.length) {
+                stack.push(encoded);
+            } else {
+                let tmp = '';
+                while(count) {
+                    tmp += encoded;
+                    count--;
+                }
+                stack.push(tmp);
+            }
+            num = '';
+            encoded = '';
+        }else {
+            stack.push(s[i]);
         }
-        count++;
+        
     }
 
-    return count;
+    for(let i = 0; i < stack.length; i++) {
+        ans += stack[i];
+    }
+    return ans;
 };
 
-console.log(integerReplacement(2147483647));
+console.log(decodeString("100[leetcode]"))
---
+++ algorithms/theory/tree/segment-tree.md
@@ -0,0 +1,8 @@
+**文本更新于 2020年05月28日 11:01:46**
+## 线段树
+
+https://blog.csdn.net/Yaokai_AssultMaster/article/details/79599809
+
+https://en.wikipedia.org/wiki/Segment_tree
+
+https://leetcode-cn.com/problems/range-sum-query-mutable/solution/qu-yu-he-jian-suo-shu-zu-ke-xiu-gai-by-leetcode/

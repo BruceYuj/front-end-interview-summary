c311f0f6a2d2b474bcc849eef647af5251e6c7cb
bruceyuj <444048170@qq.com>
2020-07-12 22:36:29 +0800

    update sth

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -8,7 +8,8 @@ module.exports = [
     {text: 'JavaScript', link: '/front-end/JavaScript/'},
     {text: 'CSS', link: '/front-end/CSS/'},
     {text: 'Vue', link: '/front-end/Vue/'},
-    {text: '《妈妈，我学不动了》', link: '/front-end/Mom-I-Cant-Learn-Anymore/'}
+    {text: '《妈妈，我学不动了》', link: '/front-end/Mom-I-Cant-Learn-Anymore/'},
+    {text: '《前端思考录》', link: '/front-end/front-end-thinking/'}
   ]}, 
   {text: '系统设计', items: [
     {text: '《为什么这么设计》', link: '/system-design/why-is-the-design/'}
---
+++ docs/front-end/front-end-thinking/README.md
@@ -0,0 +1,11 @@
+---
+title: 首页
+---
+
+## 简介
+> Front-End Thinking series（《前端思考录》）主要包含对于前端学习各个阶段的思考
+
+------
+> 遵循 MIT 协议，转载请联系作者。更多文章请关注公众号（点击下方链接）或者 Star GitHub repo.
+
+[更多文章](https://www.bruceyj.com/front-end-interview-summary/info/)

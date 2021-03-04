13f5a918dacc87ba99c66db5a0be5ef8eb82c129
yuj <444048170@qq.com>
2021-03-04 16:33:58 +0800

    update drafts

--- demo/demo.py
+++ demo/demo.py
@@ -1,40 +1,29 @@
 class Solution:
-    def maxTurbulenceSize(self, A) -> int:
-        n = len(A)
-        if n <= 1: return n
-        if n == 2:
-            if A[0] != A[1]: return n
-            else: return 1
-
-        i = 0
-        j = 1
-
-        flag = False 
-        if A[j] > A[j-1]: flag = True
-        ans = 2
-        while j+1 < n:
-            if A[j+1] > A[j] and not flag:
-                j += 1
-                flag = not flag
-                ans = max(ans, j-i+1)
-            elif A[j+1] < A[j] and flag:
-                j += 1
-                flag = not flag
-                ans = max(ans, j-i+1)
+    def numberOfSubstrings(self, s: str) -> int:
+        n = len(s)
+        l = r = 0
+        cnt = [0,0,0]
+        ans = window = 0
+        for i in range(n):
+            if i != 0:
+                ch1 = ord(s[i]) - 97
+                cnt[ch1] -= 1
+                if cnt[ch1] == 0: window -= 1
             
-            else:
-                if A[j+1] == A[j]:
-                    i = j+1
-                    j = i+1
-                else:
-                    i = j
-                    j = j+1
-                    if A[j] > A[j-1]: flag = False
-                    else: flag = True 
+            while r < n and window < 3:
+                ch = ord(s[r]) - 97
+                cnt[ch] += 1
+                if cnt[ch] == 1: window += 1
+                r += 1
+            if window == 3:
+                ans += n-r+1
         
         return ans
+            
+                
+            
 
 solution = Solution()
-ans = solution.maxTurbulenceSize([0,1,1,0,1,0,1,1,0,0])
+ans = solution.numberOfSubstrings("abcabc")
 
 print(ans)

0ee5c8a9f535cab9e0e815f620ef631f76cb61b5
yuj <444048170@qq.com>
2021-03-30 17:03:23 +0800

    drafts

--- demo/demo.py
+++ demo/demo.py
@@ -1,43 +1,30 @@
 from typing import List
 
-class Solution:
-    def numSubarraysWithSum(self, a: List[int], t: int) -> int:
-        def binary_search(l, r, x):
-            while l < r:
-                mid = (l+r) // 2
-                if p[mid] >= x: r = mid
-                else: l = mid+1
-            return l
-        
-        def binary_search1(l, r, x):
-            while l < r:
-                mid = (l+r+1) // 2
-                if p[mid] <= x: l = mid
-                else: r = mid - 1
-            return l
 
-        n = len(a)
-        p = [0] * (n+1)
 
-        for i in range(1, n+1):
-            p[i] = p[i-1] + a[i-1]
+if __name__ == "__main__":
+    
+    while True:
         
+        h = list(map(int, input().split()))
+        n = h[0]
+        
+        if n == 0: break
+        
+        a = [-1] * (n+2)
+        a[1:n+1] = h[1:]
+        
+        st = [0]
         ans = 0
-
-        for i in range(1, n+1):
-            if p[i] < t: continue
-            x = p[i] - t
-
-            l = binary_search(0, i-1, x)
-            r = binary_search1(0, i-1, x)
-            # print(l, r)
-
-            ans += (r-l+1)
-
-        return ans        
-            
-
-solution = Solution()
-ans = solution.numSubarraysWithSum([0,0,0,0,0,0,1,0,0,0], 0)
-
-print(ans)
+        for i in range(1, n+2):
+            print(i)
+            if a[st[-1]] < a[i]:
+                st.append(i)
+            else:
+                while st and a[st[-1]] >= a[i]:
+                    t = st.pop()
+                    ans = max(ans, a[t] * (i - st[-1]-1))
+                st.append(i)
+        
+        print(ans)
+    

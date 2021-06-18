2af31db24d37d0703e921f86dfdc38532cbc1518
yuj <444048170@qq.com>
2021-06-18 17:01:42 +0800

    drafts

--- demo/demo.py
+++ demo/demo.py
@@ -1,58 +1,65 @@
 
-def dfs(pa, pb, le, path):
-    global ans
+def dfs(row, s, s1, cnt, x):
     
-    if not le:
-        ans.append(path)
-        return 
-    
-    if pa == la or pb == lb: return 
-
-    for i in range(26):
-        pa1 = fa[pa][i]
-        pb1 = fb[pb][i]
+    if cnt == m:
+        y = 0
+        for i in range(m):
+            if s1[i] == 2:
+                y += 1
+                if g[row+1][i] == 'H': return
+                if i+1 < m and s1[i+1] == 2: return
+                if i+2 < m and s1[i+2] == 2: return
         
-        if pa1 == la or pb1 == lb: continue
-        dfs(pa1+1, pb1+1, le-1, chr(i+ord('a')) + path)
+        res = 0
+        
+        for i in range(m-1, -1, -1):
+            res = res * 3 + s1[i]
+        
+        f[row+1][res] = max(f[row+1][res], f[row][x] + y)
+        return 
+        
     
+    if s[cnt] == 2:
+        dfs(row,s, s1+[1], cnt+1, x)
+    elif s[cnt] == 1:
+        dfs(row, s, s1+[0], cnt+1, x)
+    else:
+        if not s1 or s1[-1] != 2:
+            dfs(row, s, s1+[2], cnt+1, x)
+        dfs(row, s, s1+[0], cnt+1, x)
+        
 
 if __name__ == "__main__":
-    sa = input()
-    sb = input()
-    la = len(sa)
-    lb = len(sb)
+    n, m = map(int, input().split())
     
-    f = [[0] * (lb+1) for i in range(la+1)]
+    M = 1
     
-    for i in range(la-1, -1, -1):
-        for j in range(lb-1, -1, -1):
-            if sa[i] == sb[j]:
-                f[i][j] = f[i+1][j+1] + 1
-            else:
-                f[i][j] = max(f[i+1][j], f[i][j+1])
-    
-    ans = []
-    
-    fa = [[la] * 26 for i in range(la+1)]
-    fb = [[lb] * 26 for i in range(lb+1)]
-    
-    for i in range(la-1, -1, -1):
-        for j in range(26):
-            if ord(sa[i]) - ord('a') == j:
-                fa[i][j] = i
-            else:
-                fa[i][j] = fa[i+1][j]
-    
-    for i in range(lb-1, -1, -1):
-        for j in range(26):
-            if ord(sb[i]) - ord('a') == j:
-                fb[i][j] = i
-            else:
-                fb[i][j] = fb[i+1][j]
+    for i in range(m):
+        M *= 3
         
-    dfs(0, 0, f[0][0], '')
-
-    for x in ans:
-        print(x)
-        
+    f = [[float("-inf")] * M for i in range(n+1)]
+    f[0][0] = 0
     
+    g = [[''] * (m) for i in range(n+1)]
+    
+    for i in range(1, n+1):
+        g[i] = input()
+    
+    for i in range(n):
+        for j in range(M):
+            state = [0] * m 
+            for k in range(m):
+                state[k] = j % 3
+                j //= 3
+            
+            
+            dfs(i, state, [], 0, j)
+    
+    ans = 0
+    
+    for i in range(M):
+        ans = max(ans, f[n][i])
+    
+    for i in range(1, n+1):
+        print(f[i])
+    print(ans)

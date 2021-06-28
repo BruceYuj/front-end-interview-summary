530d14d129b8182542402802cef3489b594f3461
bruceyuj <444048170@qq.com>
2021-06-28 19:12:39 +0800

    drafts

--- demo/demo.py
+++ demo/demo.py
@@ -1,65 +1,25 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        n = len(s)
+        m = len(part)
+        
+        if n < m: return s
+        
+        while True:
+            i = j = 0
 
-def dfs(row, s, s1, cnt, x):
-    
-    if cnt == m:
-        y = 0
-        for i in range(m):
-            if s1[i] == 2:
-                y += 1
-                if g[row+1][i] == 'H': return
-                if i+1 < m and s1[i+1] == 2: return
-                if i+2 < m and s1[i+2] == 2: return
-        
-        res = 0
-        
-        for i in range(m-1, -1, -1):
-            res = res * 3 + s1[i]
-        
-        f[row+1][res] = max(f[row+1][res], f[row][x] + y)
-        return 
-        
-    
-    if s[cnt] == 2:
-        dfs(row,s, s1+[1], cnt+1, x)
-    elif s[cnt] == 1:
-        dfs(row, s, s1+[0], cnt+1, x)
-    else:
-        if not s1 or s1[-1] != 2:
-            dfs(row, s, s1+[2], cnt+1, x)
-        dfs(row, s, s1+[0], cnt+1, x)
-        
+            while i < len(s) and j < m:
+                if s[i] != part[j]:
+                    j = 0
+                    i += 1
+                else:
+                    i += 1
+                    j += 1
+            print(i, j)
+            if j == m:
+                s = s[:i-m+1] + s[i+1:]
+            else:
+                return s
 
-if __name__ == "__main__":
-    n, m = map(int, input().split())
-    
-    M = 1
-    
-    for i in range(m):
-        M *= 3
-        
-    f = [[float("-inf")] * M for i in range(n+1)]
-    f[0][0] = 0
-    
-    g = [[''] * (m) for i in range(n+1)]
-    
-    for i in range(1, n+1):
-        g[i] = input()
-    
-    for i in range(n):
-        for j in range(M):
-            state = [0] * m 
-            for k in range(m):
-                state[k] = j % 3
-                j //= 3
-            
-            
-            dfs(i, state, [], 0, j)
-    
-    ans = 0
-    
-    for i in range(M):
-        ans = max(ans, f[n][i])
-    
-    for i in range(1, n+1):
-        print(f[i])
-    print(ans)
+so = Solution()
+ans = so.removeOccurrences("daabcbaabcbc", "abc")
